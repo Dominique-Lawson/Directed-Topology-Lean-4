8f59bbe8dba2a81f0887ec87c2bb34ab2569ee16
Dominique <drl2014@hotmail.nl>
2024-02-09 16:35:11 +0100

    Converted dTop.lean to Lean 4

--- Lean4/all.lean
+++ Lean4/all.lean
@@ -1,4 +1,5 @@
 import Lean4.constructions
+import Lean4.dTop
 import Lean4.directed_map
 import Lean4.directed_space
 import Lean4.monotone_path
---
+++ Lean4/dTop.lean
@@ -0,0 +1,64 @@
+import Lean4.constructions
+import Mathlib.CategoryTheory.ConcreteCategory.BundledHom
+import Mathlib.CategoryTheory.Elementwise
+
+/-
+  This file contains the definition of `dTopCat`, the category of directed spaces.
+  The structure of this file is based on the undirected version:
+  https://github.com/leanprover-community/mathlib4/blob/2e153a6391654fa2b82a1258cfa8c38043225969/Mathlib/Topology/Category/TopCat/Basic.lean
+-/
+
+open DirectedMap
+open CategoryTheory
+
+universe u
+
+@[to_additive existing dTopCat]
+def dTopCat : Type (u+1) := Bundled DirectedSpace
+
+namespace dTopCat
+
+-- For some reason, @DirectedMap.toFun does not work as the first argument
+instance bundledHom : BundledHom @DirectedMap :=
+ ⟨fun _ _ f => f.toFun, @DirectedMap.id, @DirectedMap.comp, @DirectedMap.coe_injective,
+  fun _ => rfl, fun _ _ _ _ _ => rfl⟩
+
+deriving instance LargeCategory for dTopCat
+
+instance concreteCategory : ConcreteCategory dTopCat := by
+  dsimp [dTopCat]
+  infer_instance
+
+instance instCoeSortdTopCatType: CoeSort dTopCat Type* := Bundled.coeSort
+
+instance directedSpaceUnbundled (x : dTopCat) : DirectedSpace x := x.str
+
+attribute [instance] ConcreteCategory.funLike in
+instance (X Y : dTopCat.{u}) : CoeFun (X ⟶ Y) fun _ => X → Y where
+  coe f := f
+
+lemma id_app (X : dTopCat.{u}) (x : ↑X) : (𝟙 X : X → X) x = x := rfl
+
+lemma comp_app {X Y Z : dTopCat.{u}}  (f : X ⟶ Y) (g : Y ⟶ Z) (x : X) :
+  (f ≫ g : X → Z) x = g (f x) := rfl
+
+/-- Construct a bundled `dTopCat` from the underlying type and the typeclass. -/
+def of (X : Type u) [DirectedSpace X] : dTopCat := ⟨X, inferInstance⟩
+
+instance directedSpace_coe (X : dTopCat) : DirectedSpace X := X.str
+
+@[reducible]
+instance directedSpace_forget (X : dTopCat) : DirectedSpace <| (forget dTopCat).obj X := X.str
+
+@[simp]
+lemma coe_of (X : Type u) [DirectedSpace X] : (of X : Type u) = X := rfl
+
+-- TODO: Change to Coe?
+instance subspace_coe {X : dTopCat} : CoeTC (Set X) dTopCat := ⟨fun s => dTopCat.of s⟩
+
+def DirectedSubtypeHom {X : dTopCat} (Y : Set X) : (dTopCat.of Y) ⟶ X :=
+  DirectedSubtypeInclusion (fun s => s ∈ Y)
+def DirectedSubsetHom {X : dTopCat} {Y₀ Y₁ : Set X} (h : Y₀ ⊆ Y₁) : (dTopCat.of Y₀) ⟶ Y₁ :=
+  DirectedSubsetInclusion h
+
+end dTopCat
