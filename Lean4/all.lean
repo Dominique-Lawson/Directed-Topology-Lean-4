08f1f934631de7b9ef44ec13bd74d48f9789f8f6
Dominique <drl2014@hotmail.nl>
2024-02-11 12:45:41 +0100

    Updated all.lean

--- Lean4/all.lean
+++ Lean4/all.lean
@@ -1,8 +1,13 @@
+import Lean4.SplitPath.split_dipath
+import Lean4.SplitPath.split_path
 import Lean4.constructions
 import Lean4.dipath
+import Lean4.directed_homotopy
 import Lean4.directed_map
 import Lean4.directed_space
 import Lean4.directed_unit_interval
 import Lean4.dTop
 import Lean4.interpolate
 import Lean4.monotone_path
+import Lean4.stretch_path
+import Lean4.unit_interval_aux
